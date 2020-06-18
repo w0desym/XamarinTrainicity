6bf64d0d4fd4fdd5208ce91b11c633a69d5f9a44
Elijah <mr.andreev2012@gmail.com>
2020-06-18 23:09:33 +0300

    final

--- XamarinTrainicity/XamarinTrainicity/App.xaml.cs
+++ XamarinTrainicity/XamarinTrainicity/App.xaml.cs
@@ -7,7 +7,7 @@ namespace XamarinTrainicity
     {
         public App()
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjYwMDczQDMxMzgyZTMxMmUzMEh1Sjl0V0Z6Ulg1Z0EyVkFZbjN1T3owcDRERGYzMGdoM3F6MzlHWHhFbEk9");
+
 
             InitializeComponent();
 
